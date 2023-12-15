678f32211d61846544e87248d468c99a050e25d9
violin <357958137@qq.com>
2023-12-15 14:40:48 +0800

    defaultHandler

--- config.go
+++ config.go
@@ -2,6 +2,7 @@ package weapon
 
 import (
 	"fmt"
+	"net/http"
 	"time"
 )
 
@@ -12,37 +13,50 @@ type Config struct {
 	PingPeriod     time.Duration // Milliseconds between pings.
 	MaxMessageSize int64         // Maximum size in bytes of a message.
 
-	connectHandler    func(*Session)
-	closeHandler      func(*Session, int, string) error
-	errorHandler      func(*Session, error)
-	messageHandler    func(*Session, int, []byte)
-	disconnectHandler func(*Session)
+	connectHandler     func(*Session)
+	closeHandler       func(*Session, int, string) error
+	errorHandler       func(*Session, error)
+	messageHandler     func(*Session, int, []byte)
+	disconnectHandler  func(*Session)
+	sessionIdGenerator func(*http.Request) string
 }
+
+var (
+	defaultConnectHandler = func(session *Session) {
+		fmt.Println("connectHandler : ", session.ID(), " ", session.Request)
+	}
+	defaultCloseHandler = func(session *Session, i int, s string) error {
+		fmt.Printf("closeHandler: id: %v, i: %v, s: %v \n", session.ID(), i, s)
+		return nil
+	}
+	defaultErrorHandler = func(_ *Session, err error) {
+		if err != nil {
+			fmt.Println("errorHandler: ", err.Error())
+		}
+	}
+	defaultMessageHandler = func(session *Session, i int, bytes []byte) {
+		fmt.Printf("Text ----- Id: %v Got msg: type: %v, data: %v\n", session.ID(), i, string(bytes))
+	}
+	defaultDisconnectionHandler = func(session *Session) {
+		fmt.Println("disconnectHandler : ", session.ID())
+	}
+	defaultSessionIdGenerator = func(req *http.Request) string {
+		return req.FormValue("uid")
+	}
+)
 
 // 默认配置
 func defaultConfig() *Config {
 	return &Config{
-		WriteWait:      10 * time.Second,
-		PongWait:       10 * time.Second,
-		PingPeriod:     time.Second * 9, //(60 * time.Second * 9) / 10,
-		MaxMessageSize: 512,
-		connectHandler: func(session *Session) {
-			fmt.Println("connectHandler : ", session.ID(), " ", session.Request)
-		},
-		closeHandler: func(session *Session, i int, s string) error {
-			fmt.Printf("closeHandler: id: %v, i: %v, s: %v \n", session.ID(), i, s)
-			return nil
-		},
-		errorHandler: func(session *Session, err error) {
-			if err != nil {
-				fmt.Println("errorHandler: ", err.Error())
-			}
-		},
-		messageHandler: func(session *Session, t int, bytes []byte) {
-			fmt.Printf("Text ----- Id: %v Got msg: %v\n", session.ID(), string(bytes))
-		},
-		disconnectHandler: func(session *Session) {
-			fmt.Println("disconnectHandler : ", session.ID())
-		},
+		WriteWait:          10 * time.Second,
+		PongWait:           10 * time.Second,
+		PingPeriod:         time.Second * 9, //(60 * time.Second * 9) / 10,
+		MaxMessageSize:     512,
+		connectHandler:     defaultConnectHandler,
+		closeHandler:       defaultCloseHandler,
+		errorHandler:       defaultErrorHandler,
+		messageHandler:     defaultMessageHandler,
+		disconnectHandler:  defaultDisconnectionHandler,
+		sessionIdGenerator: defaultSessionIdGenerator,
 	}
 }
